8b4c0a45b30eff0342343ba450fe5d65c9a6690a
Petar <94pettar@gmail.com>
2022-07-01 13:33:23 +0200

    Map Border fixed

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -4,10 +4,8 @@ import './map.css'
 const Map = () => {
   return (
     <div className='drlj__Map'>
-      <div className='drlj__Map-container'>
-      <iframe title='GoogleMap' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11202.19230918938!2d19.89372066977539!3d45.418452000000016!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x38fb47db734b72af!2sSMART%20MOVE%20FORWARD!5e0!3m2!1ssr!2srs!4v1656612864233!5m2!1ssr!2srs"></iframe>
+      <iframe title='GoogleMap' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11202.19230918938!2d19.89372066977539!3d45.418452000000016!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x38fb47db734b72af!2sSMART%20MOVE%20FORWARD!5e0!3m2!1ssr!2srs!4v1656612864233!5m2!1ssr!2srs" frameborder="0"></iframe>
       </div>
-    </div>
   )
 }
 
