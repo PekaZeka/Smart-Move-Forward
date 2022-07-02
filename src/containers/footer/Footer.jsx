8a25730047c9ffb46779309a6012bc71d67df3c0
Petar <94pettar@gmail.com>
2022-07-02 15:14:50 +0200

    Footer work in progress

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import gitHub from "../../assets/gitHub.png";
 import './footer.css'
 
 
 
 const Footer = () => {
-  const [isShown, setIsShown] = useState(true);
   return (
     <div className='drlj__Footer gradient__bg'>
-      <p>© 2022 Smart Move Forward d.o.o. All Rights Reserved.</p>
-      <div className='drlj__Footer-container'
-        onMouseEnter={() => setIsShown(false)}
-        onMouseLeave={() => setIsShown(true)}>
-        <div className='drlj__Footer-contributors'>
-        {isShown && (
-          <h3 >Contributors</h3>
-        )}
-        </div>
+      <h5>© 2022 Smart Move Forward d.o.o. All Rights Reserved.</h5>
+      <div className='drlj__Footer-container'>
+        <h3 className='tracking-out-expand'>Contributors</h3>
         <div className='drlj__Footer-fcknlegends'>
-          <div className='drlj__Footer-dalibro'>
+          <div className='drlj__Footer-dalibro bounce-in-top'>
             <a href='https://github.com/dalibro-hub'><img src={gitHub} alt='dalibro-hub img'></img></a>
             <p>dalibro-hub</p>
           </div>
-          <div className='drlj__Footer-pekazeka'>
+          <div className='drlj__Footer-pekazeka bounce-in-top-delay'>
             <a href='https://github.com/PekaZeka'><img src={gitHub} alt='PekaZeka img'></img></a>
             <p>PekaZeka</p>        
           </div>
-        
         </div> 
       </div>
     </div>
