0ea7eae7128c97c04e7f8559723cd81fc9721952
dakisha <dalibor.n.jaksic@gmail.com>
2022-07-15 16:34:32 +0200

    navbar menu icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,28 +9,41 @@ const Navbar = () => {
     threshold: 0.3,
   });
   const [toggleMenu, setToggleMenu] = useState(false);
+  
   return (
     <div ref={ref} className={inView?'smf__Navbar fade-in-fast':'smf__Navbar'}>
         
-        <div className='smf__Navbar-containers'>
-          <p className='scale-up-center'><a href="#WhatWeDo">Services</a></p>
-          <p className='scale-up-center'><a href="#Projects">Projects</a></p>
-          <p className='scale-up-center'><a href="#Contact">Contact</a></p>
-          <div className='smf__Navbar-language'>
-            <a href='#Home' >
-              <img src={lngIcon} alt="lngIcon" />
-              <p className='scale-up-center'>EN</p>
-            </a>
-          </div>
+      <div className='smf__Navbar-containers'>
+        <p className='scale-up-center'><a href="#WhatWeDo">Services</a></p>
+        <p className='scale-up-center'><a href="#Projects">Projects</a></p>
+        <p className='scale-up-center'><a href="#Contact">Contact</a></p>
+        <div className='smf__Navbar-language'>
+          <a href='#Home' >
+            <img src={lngIcon} alt="lngIcon" />
+            <p className='scale-up-center'>EN</p>
+          </a>
         </div>
-        
+      </div>
+      <div className='smf__Navbar-menu'>
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenuLine color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
-        
-        
-        
+        {toggleMenu && (
+        <div className='smf__Navbar-menu-container scale-up-center'>
+          <div className='smf__Navbar-menu_container-links'>
+            <p className='scale-up-center'><a href="#WhatWeDo">Services</a></p>
+            <p className='scale-up-center'><a href="#Projects">Projects</a></p>
+            <p className='scale-up-center'><a href="#Contact">Contact</a></p>
+            <div className='smf__Navbar-language'>
+              <a href='#Home' >
+                <p className='scale-up-center'>Language</p>
+              </a>
+            </div>
 
+          </div>
+        </div>
+        )}
+      </div>
     </div>
   )
 }
